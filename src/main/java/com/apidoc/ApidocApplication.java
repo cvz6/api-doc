cb5e124d5f1c5d439f4aeba39ee726f937a7b6e7
lp <1029538990@qq.com>
2018-04-15 16:37:06 +0800

    springboot初始化

---
+++ src/main/java/com/apidoc/ApidocApplication.java
@@ -0,0 +1,12 @@
+package com.apidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApidocApplication.class, args);
+    }
+}
