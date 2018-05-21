93570b8088f5461a6a33ed919d1b972932440ae2
lp <1029538990@qq.com>
2018-05-21 11:48:24 +0800

    增加持续化集成脚本shell

--- src/main/resources/shell/run4jenkins.sh
+++ src/main/resources/shell/run4jenkins.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
+cd /home/ci/.jenkins/workspace/apidoc/target/classes/shell
 echo "停止应用"
-#source stop.sh
-PID=$(ps -ef | grep apidoc-1.0.0.jar | grep -v grep | awk '{ print $2 }')
-if [ -z "$PID" ]
-then
-    echo Application is already stopped
-else
-    echo kill $PID
-    kill $PID
-fi
-
+source stop.sh
 echo "启动应用"
-#  source start.sh
-nohup java -jar /home/ci/.jenkins/workspace/apidoc/target/apidoc-1.0.0.jar &
+source start.sh
