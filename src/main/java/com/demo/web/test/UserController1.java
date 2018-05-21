95fb0634fc23df4fae6c9b1774cbb6bb75516a36
lp <1029538990@qq.com>
2018-05-21 12:02:35 +0800

    增加持续化集成脚本shell

--- src/main/java/com/demo/web/test/UserController1.java
+++ src/main/java/com/demo/web/test/UserController1.java
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @Author: peng.liu
  * @CreateDate: 2018/4/15 17:18
  */
-@Api(name = "测试ci持续集成", mapping = "user",order = 2)
+@Api(name = "测试ci持续集成,增加shell脚本", mapping = "user",order = 2)
 @RestController
 @RequestMapping("/user3")
 public class UserController1 {
