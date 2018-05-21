c02cbca0659446e97e16e7ca114b8b93eaf0263a
lp <1029538990@qq.com>
2018-05-21 11:20:17 +0800

    增加后台运行脚本

--- src/main/resources/shell/run.sh
+++ src/main/resources/shell/run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 echo "停止应用"
-source stop.sh
+#source stop.sh
+PID=$(ps -ef | grep apidoc-1.0.0.jar | grep -v grep | awk '{ print $2 }')
+if [ -z "$PID" ]
+then
+    echo Application is already stopped
+else
+    echo kill $PID
+    kill $PID
+fi
+
 echo "启动应用"
-source start.sh
+#  source start.sh
+nohup java -jar ../../apidoc-1.0.0.jar &
